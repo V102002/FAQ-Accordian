cc43d6cffa994929e70c0f7f366fbd6c48811a4c
Vinayak <88428501+V102002@users.noreply.github.com>
2024-07-10 10:42:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const accordians=document.querySelectorAll('.accordian');//get the accordian
-accordians.forEach(accordian=>{
-    //access the answer and the image from each accordian
-    const icon=accordian.querySelector(".icon");
-    const ans=accordian.querySelector(".answer");
-    
-    //adding a click eventListener to each accordian
-    accordian.addEventListener("click",()=>{
-        ans.classList.toggle("active"); //adding the active class based on toggle
+const accordions = document.querySelectorAll('.accordion'); // get the accordion
+const backdrop = document.querySelector('.backdrop');
 
-        //check if the accordian has been clicked
-        if(ans.classList.contains("active")){
-            icon.src="./assets/images/icon-minus.svg"
-        }else{
-            icon.src="./assets/images/icon-plus.svg"
+accordions.forEach(accordion => {
+    // access the answer and the image from each accordion
+    const icon = accordion.querySelector(".icon");
+    const ans = accordion.querySelector(".answer");
+    
+    // adding a click eventListener to each accordion
+    accordion.addEventListener("click", () => {
+        ans.classList.toggle("active"); // adding the active class based on toggle
+
+        // check if the accordion has been clicked
+        if (ans.classList.contains("active")) {
+            icon.src = "./assets/images/icon-minus.svg";
+        } else {
+            icon.src = "./assets/images/icon-plus.svg";
         }
     });
-    const backdrop=document.querySelector('.backdrop');
-    function changeBackground(){
-        if (window.innerWidth<=500) {
-            console.log(window.innerWidth);
-            backdrop.src='./assets/images/background-pattern-mobile.svg';
-        }else{
-            backdrop.src='./assets/images/background-pattern-desktop.svg';
-        }
-    }
-    //calling the function to the set the image based on current screen size
-    changeBackground();
-    window.addEventListener('resize',changeBackground);
 });
+
+function changeBackground() {
+    if (window.innerWidth <= 460) {
+        backdrop.src = './assets/images/background-pattern-mobile.svg';
+    } else {
+        backdrop.src = './assets/images/background-pattern-desktop.svg';
+    }
+}
+
+// calling the function to set the image based on current screen size
+changeBackground();
+window.addEventListener('resize', changeBackground);
